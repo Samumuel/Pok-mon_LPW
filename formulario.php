af555ab18f85ce9e583141876d06ea4cd47500db
Samuel Profeta <134728703+Samumuel@users.noreply.github.com>
2025-05-16 15:45:34 -0300

    Update formulario.php

--- formulario.php
+++ formulario.php
@@ -1,89 +1,105 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cadastro</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
+    <title>Pokémon</title>
 </head>
 
-<body>
+<body class="d-flex align-items-center justify-content-center min-vh-100 bg-light">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-6 col-lg-4">
+                <main class="form-signin w-100 m-auto text-center">
+                    <img class="mb-3" src="https://www.freeiconspng.com/uploads/pokeball-pokemon-ball-picture-11.png" alt="Pokebola" width="72" height="72">
+                    <h1 class="h3 mb-3 fw-normal">Cadastre o Pokémon</h1>
+                    <form action="card.php" method="POST">
+                        <div class="mb-3">
+                            <input type="text" class="form-control" placeholder="Nome do Pokémon" name="nome" required>
+                        </div>
 
-    <h1 class="h3 mb-3 fw-normal">Cadastre o Pokémon</h1>
-    <form action="card.php" method="POST">
+                        <div class="mb-3">
+                            <input type="text" class="form-control" placeholder="Descrição do pokémon" name="descricao" required>
+                        </div>
 
-        <input type="text" placeholder="Nome do Pokémon" name="nome">
+                        <div class="mb-3">
+                            <input type="text" class="form-control" placeholder="Informe o link da imagem" name="link" required>
+                        </div>
 
-        <br><br>
+                        <div class="mb-3">
+                            <input type="number" min="1" max="3" class="form-control" placeholder="Informe o estágio de evolução" name="evolucao" required>
+                        </div>
 
-        <input type="text" placeholder="Nome descrição do pokémon" name="descricao">
+                        <div class="mb-3">
+                            <select class="form-select" name="tipo1" required>
+                                <option value="">Selecione o tipo</option>
+                                <option value="No">Normal</option>
+                                <option value="Fo">Fogo</option>
+                                <option value="Ag">Água</option>
+                                <option value="El">Elétrico</option>
+                                <option value="Gr">Grama</option>
+                                <option value="Ge">Gelo</option>
+                                <option value="Lu">Lutador</option>
+                                <option value="Ve">Veneno</option>
+                                <option value="Te">Terra</option>
+                                <option value="Vo">Voador</option>
+                                <option value="Ps">Psíquico</option>
+                                <option value="In">Inseto</option>
+                                <option value="Fa">Fada</option>
+                                <option value="Dr">Dragão</option>
+                                <option value="Me">Metal</option>
+                            </select>
+                        </div>
 
-        <br><br>
+                        <div class="mb-3">
+                            <select class="form-select" name="tipo2" required>
+                                <option value="">Selecione o tipo</option>
+                                <option value="No">Normal</option>
+                                <option value="Fo">Fogo</option>
+                                <option value="Ag">Água</option>
+                                <option value="El">Elétrico</option>
+                                <option value="Gr">Grama</option>
+                                <option value="Ge">Gelo</option>
+                                <option value="Lu">Lutador</option>
+                                <option value="Ve">Veneno</option>
+                                <option value="Te">Terra</option>
+                                <option value="Vo">Voador</option>
+                                <option value="Ps">Psíquico</option>
+                                <option value="In">Inseto</option>
+                                <option value="Fa">Fada</option>
+                                <option value="Dr">Dragão</option>
+                                <option value="Me">Metal</option>
+                                <option value="Nenhum">Não Tem</option>
+                            </select>
+                        </div>
 
-        <input type="text" placeholder="Informe o link da imagem" name="link">
-
-        <br><br>
-
-        <input type="number" placeholder="Informe o estágio de evolução" name="evolucao">
-
-        <br><br>
-
-        <select name="tipo1">
-            <option value="">Selecione o tipo</option>
-            <option value="No">Normal</option>
-            <option value="Fo">Fogo</option>
-            <option value="Ag">Água</option>
-            <option value="El">Elétrico</option>
-            <option value="Gr">Grama</option>
-            <option value="Ge">Gelo</option>
-            <option value="Lu">Lutador</option>
-            <option value="Ve">Veneno</option>
-            <option value="Te">Terra</option>
-            <option value="Vo">Voador</option>
-            <option value="Ps">Psíquico</option>
-            <option value="In">Inseto</option>
-            <option value="Fa">Fada</option>
-            <option value="Dr">Dragão</option>
-            <option value="Me">Metal</option>
-        </select>
-
-        <br><br>
-
-        <select name="tipo2">
-            <option value="">Selecione o tipo</option>
-            <option value="No">Normal</option>
-            <option value="Fo">Fogo</option>
-            <option value="Ag">Água</option>
-            <option value="El">Elétrico</option>
-            <option value="Gr">Grama</option>
-            <option value="Ge">Gelo</option>
-            <option value="Lu">Lutador</option>
-            <option value="Ve">Veneno</option>
-            <option value="Te">Terra</option>
-            <option value="Vo">Voador</option>
-            <option value="Ps">Psíquico</option>
-            <option value="In">Inseto</option>
-            <option value="Fa">Fada</option>
-            <option value="Dr">Dragão</option>
-            <option value="Me">Metal</option>
-            <option value="Nenhum">Não Tem</option>
-        </select>
-
-        <br><br>
-        <br>
-        <button style="color: green;">Enviar</button>
-    </form>
-
-
+                        <div class="text-center">
+                            <button type="submit" class="btn btn-success">Enviar</button>
+                        </div>
+                    </form>
+                </main>
+            </div>
+        </div>
+    </div>
 </body>
 
 </html>
 
-<img class="mb-4" src="https://e7.pngegg.com/pngimages/337/240/png-clipart-pokeball-pokemon-battle-revolution-pikachu-entei-pokeball-sphere-pokemon-thumbnail.png" alt="" width="72" height="57">
-<h1 class="h3 mb-3 fw-normal">Please sign in</h1>
-<div class="form-floating"> <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com"> <label for="floatingInput">Email address</label> </div>
-<div class="form-floating"> <input type="password" class="form-control" id="floatingPassword" placeholder="Password"> <label for="floatingPassword">Password</label> </div>
-<div class="form-check text-start my-3"> <input class="form-check-input" type="checkbox" value="remember-me" id="checkDefault"> <label class="form-check-label" for="checkDefault">
-        Remember me
-    </label> </div>
-<button class="btn btn-primary w-100 py-2" type="submit">Sign in</button>
+<style>
+    @font-face {
+        font-family: 'Pokemon Classic';
+        src: url('fonts/Pokemon-Classic.ttf') format('truetype');
+        font-weight: normal;
+        font-style: normal;
+    }
+
+    body {
+        font-family: 'Pokemon Classic', sans-serif;
+        background-image: url("https://pokedle.net/img/Background.b373eb68.png");
+        background-position: center top;
+        background-attachment: fixed;
+        background-size: cover;
+    }
+</style>
